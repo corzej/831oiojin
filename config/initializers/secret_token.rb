74676f02c816f01cd5bd70354e8b9a39e84f12f9
corzej <corzej@gmail.com>
2013-11-22 11:21:31 -0500

    AddInitial with rspec and foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Com831oiojinZurb::Application.config.secret_key_base = '5b1379f46d755c341b7fb2911ab4c0a07fdacda8dde51c27a96f4ba226cc5b9e7f3ae4eff5da5ca8df41b7ebe2b3a803c215549df9dd60f580040702f1f7b441'
